jstmn: added '<klampt use_vis_geom="1"/>', debug prints to assert_pose_positions_almost_equal(). TODO: fix fk errors for Ur robots. currently there is a flipped sign on the position

tests/math_utils_test.py
@@ -9,7 +9,7 @@ from jrl.math_utils import (
     quaternion_conjugate,
     quaternion_norm,
     geodesic_distance_between_quaternions,
-    geodesic_distance_between_quaternions_warp,
+    # geodesic_distance_between_quaternions_warp,
     quatconj,
     quaternion_product,
     quatmul,
@@ -173,28 +173,28 @@ class TestConversions(unittest.TestCase):
         self.assertAlmostEqual(3.1415927, returned[0].item(), places=3)
         self.assertAlmostEqual(0.25, returned[1].item(), places=6)
 
-    def test_geodesic_distance_between_quaternions_warp(self):
-        """Test geodesic_distance_between_quaternions with torch tensor inputs"""
-        q1 = torch.tensor([[1.0, 0.0, 0.0, 0.0]], device="cpu", dtype=torch.float32)
-        # Rotation about +x axis by .25 radians
-        q2 = torch.tensor([[0.9921977, 0.1246747, 0, 0]], device="cpu", dtype=torch.float32)
-        returned = geodesic_distance_between_quaternions_warp(q1, q2)[0].item()
-        self.assertAlmostEqual(0.25, returned, places=6)
+    # def test_geodesic_distance_between_quaternions_warp(self):
+    #     """Test geodesic_distance_between_quaternions with torch tensor inputs"""
+    #     q1 = torch.tensor([[1.0, 0.0, 0.0, 0.0]], device="cpu", dtype=torch.float32)
+    #     # Rotation about +x axis by .25 radians
+    #     q2 = torch.tensor([[0.9921977, 0.1246747, 0, 0]], device="cpu", dtype=torch.float32)
+    #     returned = geodesic_distance_between_quaternions_warp(q1, q2)[0].item()
+    #     self.assertAlmostEqual(0.25, returned, places=6)
 
-        # Test 2
-        q1 = torch.tensor([[1.0, 0.0, 0.0, 0.0]], device="cpu", dtype=torch.float32)
-        q2 = torch.tensor([[0.0, 0.92387953, 0.38268343, 0.0]], device="cpu", dtype=torch.float32)
-        returned = geodesic_distance_between_quaternions_warp(q1, q2)[0].item()
-        self.assertAlmostEqual(3.1415927, returned, places=3)
+    #     # Test 2
+    #     q1 = torch.tensor([[1.0, 0.0, 0.0, 0.0]], device="cpu", dtype=torch.float32)
+    #     q2 = torch.tensor([[0.0, 0.92387953, 0.38268343, 0.0]], device="cpu", dtype=torch.float32)
+    #     returned = geodesic_distance_between_quaternions_warp(q1, q2)[0].item()
+    #     self.assertAlmostEqual(3.1415927, returned, places=3)
 
-        # Test 3
-        q1 = torch.tensor([[1.0, 0.0, 0.0, 0.0], [1.0, 0.0, 0.0, 0.0]], device="cpu", dtype=torch.float32)
-        q2 = torch.tensor(
-            [[0.0, 0.92387953, 0.38268343, 0.0], [0.9921977, 0.1246747, 0, 0]], device="cpu", dtype=torch.float32
-        )
-        returned = geodesic_distance_between_quaternions_warp(q1, q2)
-        self.assertAlmostEqual(3.1415927, returned[0].item(), places=3)
-        self.assertAlmostEqual(0.25, returned[1].item(), places=6)
+    #     # Test 3
+    #     q1 = torch.tensor([[1.0, 0.0, 0.0, 0.0], [1.0, 0.0, 0.0, 0.0]], device="cpu", dtype=torch.float32)
+    #     q2 = torch.tensor(
+    #         [[0.0, 0.92387953, 0.38268343, 0.0], [0.9921977, 0.1246747, 0, 0]], device="cpu", dtype=torch.float32
+    #     )
+    #     returned = geodesic_distance_between_quaternions_warp(q1, q2)
+    #     self.assertAlmostEqual(3.1415927, returned[0].item(), places=3)
+    #     self.assertAlmostEqual(0.25, returned[1].item(), places=6)
 
     def test_quaternion_to_rotation_matrix(self):
         """Test quaternion_to_rotation_matrix()"""